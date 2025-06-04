cf3e51870f3a76214a9f41ba5099094acf182fcb
Renan <rsiqueira78@gmail.com>
2025-06-04 15:23:01 -0300

    criar o app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+let qtdNumeros = document.getElementById('quantidade');
+let de = document.getElementById('de');
+let ate = document.getElementById('ate');
+
+
+function sortear(){
+    window.alert(`Quantidade de números: ${qtdNumeros.value}`);
+    window.alert(`Do número: ${de.value}`);
+    window.alert(`Até o número: ${ate.value}`);
+}
